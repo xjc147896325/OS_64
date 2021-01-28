a8c0ea8aed1d49665e57a83e6aec1422f75fe7b6
jOKERII <631854721@qq.com>
2021-01-28 23:47:13 +0800

    久违的上线干活

--- kernel/kernel/main.c
+++ kernel/kernel/main.c
@@ -1,4 +1,42 @@
 void Start_Kernel(void)
 {
+	int* addr = (int*)0xffff800000a00000;
+	int i = 0;
+	
+	for(i = 0; i < 20*1440; i++)
+	{
+		*((char*)addr + 0) = 0x00;
+		*((char*)addr + 1) = 0x00;
+		*((char*)addr + 2) = 0xff;
+		*((char*)addr + 3) = 0x00;
+		addr++;
+	}
+		
+	for(i = 0; i < 20*1440; i++)
+	{
+		*((char*)addr + 0) = 0x00;
+		*((char*)addr + 1) = 0xff;
+		*((char*)addr + 2) = 0x00;
+		*((char*)addr + 3) = 0x00;
+		addr++;
+	}
+	
+	for(i = 0; i < 20*1440; i++)
+	{
+		*((char*)addr + 0) = 0xff;
+		*((char*)addr + 1) = 0x00;
+		*((char*)addr + 2) = 0x00;
+		*((char*)addr + 3) = 0x00;
+		addr++;
+	}	
+	
+	for(i = 0; i < 20*1440; i++)
+	{
+		*((char*)addr + 0) = 0xff;
+		*((char*)addr + 1) = 0xff;
+		*((char*)addr + 2) = 0xff;
+		*((char*)addr + 3) = 0x00;
+		addr++;
+	}
 	while(1);
 }
